bd064f709387ba8a72a072c3ec0d893f453ad68b
Gunski <gunnarkryth@gmail.com>
2024-09-27 09:38:34 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react";
 import "./assets/styles/App.scss";
-import { Home } from "./pages/Home";
 
 function App() {
-  <BrowserRouter>
-  <Router>
-    <Route>
-      
-    </Route>
-  </Router>
-  </BrowserRouter>
+  
   return (
-    <Wrapper>
-      <Atom></Atom>
-    </Wrapper>
+    <>
+    
+    </>
   );
 }
 
